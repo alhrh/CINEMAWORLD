58da58e7e16968cb376a98d481c9a71a5d7f574c
hussam ALZHRANI <alhrh3@gmail.com>
2025-04-16 14:15:25 +0300

    نقل ملفات الموقع إلى مجلد frontend

---
+++ frontend/script.js
@@ -0,0 +1,13 @@
+function openModal(id) {
+    const modal = document.getElementById("modal-" + id);
+    if (modal) {
+      modal.style.display = "block";
+    }
+  }
+  
+  function closeModal() {
+    const modals = document.querySelectorAll(".modal");
+    modals.forEach(modal => {
+      modal.style.display = "none";
+    });
+  }
